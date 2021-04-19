0c6ce1202dd9616e795298af7e400cba1de76f5a
bhh <onetwo@nyist.edu.cn>
2021-04-19 12:20:33 +0800

    init

---
+++ src/main/java/com/bhh/openclose/ICourse.java
@@ -0,0 +1,14 @@
+package com.bhh.openclose;
+
+
+/**
+ * @author bhh
+ * @description 接口以实现部分方法定义
+ * @date Created in 2021-04-19 11:05
+ * @modified By
+ */
+public interface ICourse {
+    Integer getId();
+    String getName();
+    Double getPrice();
+}
